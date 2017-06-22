f411388baa4e580696a4fae6e328dd6917cd68d4
Chris Dostert <chrisdostert@users.noreply.github.com>
2017-06-22 12:36:28 -0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-SSH_KEY_DIR=~/.ssh
-SSH_KEY_FILE_PATH="${SSH_KEY_DIR}/id_rsa"
-
-echo "setting .ssh creds"
-mkdir -p ${SSH_KEY_DIR}
-echo "$sshKey" > $SSH_KEY_FILE_PATH
-chmod 600 ${SSH_KEY_FILE_PATH}
+echo "$sshKey" > /sshKey
+chmod 600 /sshKey
 
 echo "establishing ssh tunnel to k8s master"
-ssh -4 -o StrictHostKeyChecking=no -NfL 8080:localhost:8080 "${sshUsername}@${hostname}"
+ssh -4 -i /sshKey -o StrictHostKeyChecking=no -NfL 8080:localhost:8080 "${sshUsername}@${hostname}"
 
 echo "performing k8s deployment"
 kubectl apply -f /deployment.yml --namespace "$namespace"
